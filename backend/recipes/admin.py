2faf41499d95b062a23b98c89e4a5aacc56d7073
Eduard Lozhkin <lozhkinea@ya.ru>
2022-08-24 08:05:31 +0300

    added models to the admin site

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -2,9 +2,57 @@ from django.contrib import admin
 
 from .models import Favotites, Ingredient, Recipe, ShoppingCart, Subscribe, Tag
 
-admin.site.register(Favotites)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
+
+class FavotitesAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'recipe',
+    )
+    search_fields = (
+        'user',
+        'recipe',
+    )
+    list_filter = (
+        'user',
+        'recipe',
+    )
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    search_fields = ('name', 'author')
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = '-пусто-'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color',
+        'slug',
+    )
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Favotites, FavotitesAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(ShoppingCart)
 admin.site.register(Subscribe)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'is_superuser',
+    )
+    search_fields = ('username', 'email')
+    list_filter = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, UserAdmin)
