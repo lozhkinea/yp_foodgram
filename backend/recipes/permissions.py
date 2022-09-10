11198e4aaac8395613f68acc135cac6d1651d5c6
Eduard Lozhkin <lozhkinea@ya.ru>
2022-09-10 05:15:07 +0300

    first deploy

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -13,7 +13,11 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.is_staff
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_staff
+        )
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
