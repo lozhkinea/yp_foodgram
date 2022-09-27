7f1501919c440c2b3287f23badc6bb3ea308b3f2
Eduard Lozhkin <lozhkinea@ya.ru>
2022-09-28 00:00:46 +0300

    added subscriptions tests

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -27,6 +27,18 @@ class UserBaseTestCase(APITestCase):
     def _unauthorize(self):
         self.client.credentials()
 
+    def _create_users(self, count):
+        ids = []
+        for i in range(count):
+            data = {
+                **self.user_data,
+                'username': f'user{i}',
+                'email': f'user{i}@ya.ru',
+            }
+            response = self.client.post(self.url, data)
+            ids.append(response.data['id'])
+        return ids
+
 
 class UserRegistrationTestCase(UserBaseTestCase):
     @classmethod
@@ -148,40 +160,34 @@ class UserDetailTestCase(UserBaseTestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-class ManyUserTestCase(UserBaseTestCase):
+class SeveralUsersTestCase(UserBaseTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.RESPONSE_DATA_LENGTH = 4
         cls.RESULT_DETAIL_LENGTH = 6
         cls.IS_SUBSCRIBED_DEFAULT = False
         cls.PAGE_SIZE = 6
         cls.USERS_COUNT = 7
+        cls.FEW_USERS_COUNT = 2
+        cls.LIMIT_SIZE = 3
         cls.data_keys = ('count', 'next', 'previous', 'results')
+        cls.pages = {1: 3, 2: 3, 3: 2}
 
     def setUp(self):
         response = self.client.post(self.url, self.user_data)
         self.user_id = response.data['id']
         self._authorize()
 
-    def _create_users(self, count):
-        for i in range(count):
-            data = {
-                **self.user_data,
-                'username': f'user{i}',
-                'email': f'user{i}@ya.ru',
-            }
-            self.client.post(self.url, data)
-
     def test_few_users_list(self):
-        few_count = 2
-        self._create_users(few_count)
+        self._create_users(self.FEW_USERS_COUNT)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), len(self.data_keys))
         for key in self.data_keys:
             self.assertIn(key, response.data)
-        self.assertEqual(len(response.data['results']), few_count + 1)
+        self.assertEqual(
+            len(response.data['results']), self.FEW_USERS_COUNT + 1
+        )
 
     def test_users_list(self):
         self._create_users(self.USERS_COUNT)
@@ -193,14 +199,20 @@ class ManyUserTestCase(UserBaseTestCase):
         self.assertEqual(len(response.data['results']), self.PAGE_SIZE)
 
     def test_users_list_limit(self):
-        limit_size = 5
         self._create_users(self.USERS_COUNT)
-        response = self.client.get(f'{self.url}?limit={limit_size}')
+        response = self.client.get(f'{self.url}?limit={self.LIMIT_SIZE}')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), len(self.data_keys))
         for key in self.data_keys:
             self.assertIn(key, response.data)
-        self.assertEqual(len(response.data['results']), limit_size)
+        self.assertEqual(len(response.data['results']), self.LIMIT_SIZE)
+        for page, size in self.pages.items():
+            response = self.client.get(f'{self.url}?page={page}&limit={size}')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(response.data), len(self.data_keys))
+            for key in self.data_keys:
+                self.assertIn(key, response.data)
+            self.assertEqual(len(response.data['results']), size)
 
     def test_users_list_result_detail(self):
         response = self.client.get(self.url)
@@ -219,6 +231,16 @@ class SubscriptionTestCase(UserBaseTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.RESULT_DETAIL_LENGTH = 8
+        cls.IS_SUBSCRIBED_DEFAULT = False
+        cls.PAGE_SIZE = 6
+        cls.USERS_COUNT = 7
+        cls.FEW_USERS_COUNT = 2
+        cls.LIMIT_SIZE = 3
+        cls.NON_EXIST_USER_ID = 999
+        cls.url_subscriptons = cls.url + 'subscriptions/'
+        cls.data_keys = ('count', 'next', 'previous', 'results')
+        cls.pages = {1: 3, 2: 3, 3: 2}
         cls.second_user_data = {
             'email': 'second@yandex.ru',
             'username': 'second',
@@ -226,25 +248,22 @@ class SubscriptionTestCase(UserBaseTestCase):
             'last_name': 'Пупкин',
             'password': 'Qwerty!2',
         }
-        cls.third_user_data = {
-            'email': 'third@yandex.ru',
-            'username': 'third',
-            'first_name': 'Вася',
-            'last_name': 'Пупкин',
-            'password': 'Qwerty!2',
-        }
+
+    def _subscribe(self, ids):
+        self._authorize()
+        for id in ids:
+            self.client.post(
+                reverse('users:user-detail', kwargs={'id': id}) + 'subscribe/'
+            )
 
     def setUp(self):
         response = self.client.post(self.url, self.user_data)
         self.user_id = response.data['id']
         response = self.client.post(self.url, self.second_user_data)
         self.second_user_id = response.data['id']
-        response = self.client.post(self.url, self.third_user_data)
-        self.third_user_id = response.data['id']
-        self.non_exist_user_id = self.third_user_id + 1
-        self._authorize()
 
     def test_authorized_subscribe(self):
+        self._authorize()
         url = (
             reverse('users:user-detail', kwargs={'id': self.second_user_id})
             + 'subscribe/'
@@ -259,16 +278,82 @@ class SubscriptionTestCase(UserBaseTestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         response = self.client.post(
-            reverse('users:user-detail', kwargs={'id': self.non_exist_user_id})
+            reverse('users:user-detail', kwargs={'id': self.NON_EXIST_USER_ID})
             + 'subscribe/'
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_unauthorized_subscribe(self):
+        self._unauthorize()
         url = (
             reverse('users:user-detail', kwargs={'id': self.second_user_id})
             + 'subscribe/'
         )
-        self.client.credentials()
         response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authorized_subscriptions(self):
+        self._authorize()
+        response = self.client.get(self.url_subscriptons)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), len(self.data_keys))
+        for key in self.data_keys:
+            self.assertIn(key, response.data)
+        self.assertEqual(len(response.data['results']), 0)
+        ids = self._create_users(self.USERS_COUNT)
+        self._subscribe(ids)
+        response = self.client.get(self.url_subscriptons)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), self.PAGE_SIZE)
+
+    def test_unauthorized_subscriptions(self):
+        self._unauthorize()
+        response = self.client.get(self.url_subscriptons)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_subscriptions_limit(self):
+        ids = self._create_users(self.USERS_COUNT)
+        self._subscribe(ids)
+        response = self.client.get(
+            f'{self.url_subscriptons}?limit={self.LIMIT_SIZE}'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), len(self.data_keys))
+        for key in self.data_keys:
+            self.assertIn(key, response.data)
+        self.assertEqual(len(response.data['results']), self.LIMIT_SIZE)
+        for page, size in self.pages.items():
+            response = self.client.get(
+                f'{self.url_subscriptons}?page={page}&limit={size}'
+            )
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(response.data), len(self.data_keys))
+            for key in self.data_keys:
+                self.assertIn(key, response.data)
+            self.assertEqual(len(response.data['results']), size)
+
+    def test_subscriptions_detail(self):
+        self._subscribe([self.second_user_id])
+        response = self.client.get(self.url_subscriptons)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = response.data['results'][0]
+        self.assertEqual(len(result), self.RESULT_DETAIL_LENGTH)
+        for key in self.data:
+            self.assertIn(key, result)
+            self.assertEqual(result[key], self.second_user_data[key])
+        self.assertIn('id', result)
+        self.assertEqual(result['id'], self.second_user_id)
+        self.assertIn('is_subscribed', result)
+        self.assertEqual(result['is_subscribed'], True)
+        self.assertIn('recipes', result)
+        self.assertEqual(len(result['recipes']), 0)
+        self.assertIn('recipes_count', result)
+        self.assertEqual(len(result['recipes']), result['recipes_count'])
+
+    def test_subscriptions_detail_resipes(self):
+        self._subscribe([self.second_user_id])
+        response = self.client.get(self.url_subscriptons)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = response.data['results'][0]
+        recipes = result['recipes']
+        self.assertEqual(len(recipes), result['recipes_count'])
